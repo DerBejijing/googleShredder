3679e4f70a31682b810ba7c2f28bf2a900b2fea5
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2021-03-20 16:02:16 +0100

    Update Shredder.py
    Language support, English version

--- src/shredder.py
+++ src/shredder.py
@@ -4,16 +4,15 @@ from os import name, system
 import pycountry
 import sys
 import math
+import time
 
-trPath = ['']
+path = ['']
 progress = 0
-
-tr = Translator()
-print("[*] Starting...")
+target_language = 'de'
 
 def progressBar(currentPosition):
     global progress
-    totalItems = len(trPath)
+    totalItems = len(path)
     return math.floor(((currentPosition) / totalItems) * 100)
 
 def clear():
@@ -24,8 +23,8 @@ def clear():
 
 def trashText():
     textToTranslate = str(InputText.get("1.0",END))
-    print(trPath)
-    for lang in trPath:
+    print(path)
+    for lang in path:
         try:
             text = tr.translate(textToTranslate, dest=lang)
             textToTranslate = text.text
@@ -33,7 +32,7 @@ def trashText():
             pass
 
         clear()
-        print("[*] Progress: " + str(progressBar(trPath.index(lang))) + "%")
+        print("[*] Progress: " + str(progressBar(path.index(lang))) + "%")
 
     clearOut()
     OutputText.insert(END, textToTranslate)
@@ -43,30 +42,29 @@ def trashText():
 
 def trashTextNormal():
     print("[*] using normal destruction mode")
-    global trPath
-    trPath = ['ar','la','ja','de']
+    global path
+    path = ['ar','la','ja']
+    path.append(target_language)
     trashText()
-
 
 def trashTextComplex():
     print("[*] using complex destruction mode")
-    global trPath
-    trPath = ['']
+    global path
+    path = ['']
     try:
         for country in pycountry.countries:
-            trPath.append(str.lower(country.alpha_2))
+            path.append(str.lower(country.alpha_2))
     except Exception as e:
-        print("")
-        print("[!] Error occured whilst generating translation path!")
-        print("[!] Make sure to have module 'pycountry' installed!")
-        print("[!] If the error remains, please contact the developer")
-
+        raise
 
     print("[*] Path generated!")
-    trPath = list(dict.fromkeys(trPath))
-    trPath.remove('de')
-    trPath.append('de')
+    path = list(dict.fromkeys(path))
+    try:
+        path.remove(target_language)
+    except Exception as e:
+        pass
 
+    path.append(target_language)
     trashText()
 
 def clearOut():
@@ -79,24 +77,56 @@ def clearAll():
 def quitProgramm():
     sys.exit(0)
 
+def setTargetLang(language):
+    global target_language
+    target_language = language
+    OutputLabel.config(text="Output (" + language + "):")
+    print("[*] Target language changed to: " + language)
+
+def help():
+    print("[*] Helping")
+
+tr = Translator()
+print("[*] Starting")
+
 window = Tk()
+menu = Menu(window)
 window.title("Enhance your understanding of grammar")
 window.config(bg="orange red")
+window.config(menu=menu)
+window.resizable(False, False)
 
-InfoLabel = Label(window, text="Enhance your knowledge about grammar", anchor=W, justify=LEFT, bg="orange red", fg= "white", font="none 24 bold").grid(row=0,column=0)
-DescLabel = Label(window, text="Gib hier einen (grammatikalisch korrekten, DEUTSCHEN) Text ein:", anchor=W, justify=LEFT, bg="orange red", fg="white", font="none 12 bold").grid(row=1,column=0)
+editMenu = Menu(menu)
+menu.add_cascade(label="edit", menu=editMenu)
+
+editMenu.add_command(label="clear fields", command=clearAll)
+editMenu.add_command(label="help...", command=help)
+editMenu.add_separator()
+editMenu.add_command(label="quit", command=quitProgramm)
+
+targetMenu = Menu(menu)
+menu.add_cascade(label="target language", menu=targetMenu)
+
+targetMenu.add_command(label="English (en)", command= lambda: setTargetLang("en"))
+targetMenu.add_command(label="Deutsch (de)", command= lambda: setTargetLang("de"))
+targetMenu.add_command(label="Français (fr)", command= lambda: setTargetLang("fr"))
+targetMenu.add_command(label="Español (es)", command= lambda: setTargetLang("es"))
+
+destructMenu = Menu(menu)
+menu.add_cascade(label="destruct", menu=destructMenu)
+
+destructMenu.add_command(label="Normal", command=trashTextNormal)
+destructMenu.add_command(label="Complex", command=trashTextComplex)
+
+InfoLabel = Label(window, text="Enhance your knowledge about grammar", anchor=W, justify=LEFT, bg="orange red", fg="white", font="none 24 bold").grid(row=0,column=0)
+DescLabel = Label(window, text="Enter some text:", anchor=W, justify=LEFT, bg="orange red", fg="white", font="none 12 bold").grid(row=1,column=0)
 InputText = Text(window, height=10, width=50)
 InputText.grid(row=2,column=0)
-line1 = Label(window, text="\n\n").grid(row=3,column=0)
-trashButton = Button(window, text="Text Schrotten", command=trashTextNormal).grid(row=4,column=0)
-trashButtonComplex = Button(window, text="Text Komplex Schrotten", command=trashTextComplex).grid(row=5,column=0)
-clearButton = Button(window, text="Löschen", command=clearAll).grid(row=6,column=0)
-line2 = Label(window, text="\n\n").grid(row=7,column=0)
+OutputLabel = Label(window, text="Output (" + target_language + "):", anchor=W, justify=LEFT, bg="orange red", fg="white", font="none 12 bold")
+OutputLabel.grid(row=3,column=0)
 OutputText = Text(window, height=10, width=50)
-OutputText.grid(row=8,column=0)
-nothing = Label(window, text="\n\n\n", bg="orange red", fg="orange red").grid(row=9,column=0)
-
-quitButton = Button(window, text="Exit", bg="red", fg="black", command=quitProgramm).grid(row=10,column=0)
-
+OutputText.grid(row=4,column=0)
+nothing = Label(window, text="\n\n\n", bg="orange red", fg="orange red").grid(row=5,column=0)
+quitButton = Button(window, text="Exit", bg="red", fg="black", command=quitProgramm).grid(row=6,column=0)
 
 window.mainloop()
